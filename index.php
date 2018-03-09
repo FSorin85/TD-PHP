2accee2a80ff7b730a5b0d47a0fe524e09d084b2
FSorin85 <florian.sorin85@gmail.com>
2018-03-09 14:03:27 +0100

    modification d'index, des erreurs sont toujours présentes

--- index.php
+++ index.php
@@ -1,9 +1,47 @@
 <?php
 
-$hote = 'localhost';
-$port = 3306;
-$db = 'php_is_best';
-$login = 'root';
-$mdp = '';
-$pdo = new PDO('mysql:host='.$hote.';port='. $port.
-        ';dbname='.$db, $login, $mdp);
+
+try {
+
+    $hote = 'localhost';
+    $port = 3306;
+    $db = 'php_is_best';
+    $login = 'root';
+    $mdp = '';
+    $pdo = new PDO('mysql:host=' . $hote . ';port=' . $port .
+        ';dbname=' . $db, $login, $mdp);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    var_dump($e->getMessage());
+}
+//ajout username
+$string1 = str_replace('-', '', $argv[1]);
+$stringExploded1 = explode('=', $string1);
+$usernameOptions = ['username', 'u'];
+if(in_array($stringExploded1[0], $usernameOptions)) {
+    $user=$stringExploded1[1];
+    $pdo->exec("INSERT INTO users (username)VALUES ($user);");
+}
+
+//ajout email
+$string2 = str_replace('-', '', $argv[2]);
+$stringExploded2 = explode('=', $string2);
+$emailOptions = ['email', 'e'];
+if(in_array($stringExploded2[0], $emailOptions)) {
+    $email=$stringExploded2[1];
+    $pdo->exec("INSERT INTO users (username)VALUES ($email);");
+}
+
+//ajout note
+$string3 = str_replace('-', '', $argv[3]);
+$stringExploded3 = explode('=', $string3);
+$noteOptions = ['note', 'n'];
+if(in_array($stringExploded3[0], $noteOptions)) {
+    $note = $stringExploded3[1];
+    $pdo->exec("INSERT INTO users (username)VALUES ($note);");
+}
+
+
+
+
+
